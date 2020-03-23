be88028c623d08b134d0e994fbe681afa745ab0f
YasiTL <yasaman.most@gmail.com>
2020-03-23 15:42:18 -0500

    add staff component

--- src/components/whosHere/whosHere.js
+++ src/components/whosHere/whosHere.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import Staff from '../whosHere/staff/staff';
 
 export default class WhosHere extends Component {
     render() {
         return (
-            <h1>Who's Here</h1>
+            <div>
+                <h1>Who's Here</h1>
+                <React.Fragment>
+                    <Staff/>
+                </React.Fragment>
+            </div>
         )
     }
 }
