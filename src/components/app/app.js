1d9838a7d450ed769f8516b6399cc87bdf139c68
Taylor Berg <me@tman0.com>
2020-03-23 17:29:34 -0400

    working socket.io & authentication

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
-import { Container, Col } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import io from 'socket.io-client';
 import GlobalNav from '../globalNav/globalNav';
-import './app.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import WhosHere from '../whosHere/whosHere';
 import { Index } from "../pages/index/index";
+import WhosHere from '../whosHere/whosHere';
+import './app.css';
+
 
 
 export default class App extends Component {
     constructor() {
         super();
+        this.endpoint = 'http://localhost:5000/v2'
         this.state = {
+            hardware_id: 901,
             location: {
                 name: "Assembly Space",
-                id: "2",
+                id: 2,
                 token: ""
             },
             labState: {
-                staffInLab: [],
-                studentsInLab: []
+                activeUsers: []
             }
         };
     }
 
     componentDidMount() {
-        
+        localStorage.debug = 'socket.io-client:manager';
+        const socket = io(this.endpoint)
+
+        socket.on('connect', () => {
+            // if we already have a token, attempt a reauth
+            if (this.state.location.token) {
+                socket.emit('reauth', {
+                    'location_id': this.state.location.id,
+                    'hardware_id': this.state.hardware_id,
+                    'token': this.state.location.token
+                });
+            } else {
+                // replace with the actual secret (but don't forget to change it back!), at least until auth component is done
+                socket.emit('auth', {
+                    'location_id': 2,
+                    'hardware_id': 901,
+                    'secret': 'blarg'
+                })
+            }
+        });
+        socket.on('reconnect', () => {
+
+        });
+        socket.on('auth_success', (data) => {
+            this.setState(data.initial_state);
+        });
+        socket.connect();
     }
 
     render() {
@@ -38,6 +67,7 @@ export default class App extends Component {
                     <GlobalNav location={this.state.location.name} />
                     <Switch>
                         <Route path="/auth">
+                            {this.state.location.token && <Redirect to="/" />}
                             <Container id="layoutContainer">
                                 <div id="content">
                                     <div>
@@ -48,7 +78,7 @@ export default class App extends Component {
                         </Route>
                         <Route path="/"> {/* matches anything */}
                             {/* if token is empty or null, redirect to /auth */}
-                            {/*!this.state.location.token && <Redirect to="/auth" />*/}
+                            {!this.state.location.token && <Redirect to="/auth" />}
                             <Container fluid={true} id="layoutContainer">
                                 <Col sm="8" lg="10" id="content">
                                     <Switch>
