4c42453a7b4c974655b2a2d51c9eddddd8280c2d
YasiTL <yasaman.most@gmail.com>
2020-03-25 15:36:05 -0500

    seperate dummy staff and image into 2 cols

--- src/components/whosHere/staff/staff-user/staff-user.js
+++ src/components/whosHere/staff/staff-user/staff-user.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import { Image } from 'react-bootstrap';
+import { Image, Container, Row, Col } from 'react-bootstrap';
 
 export default class StaffUser extends Component {
     render() {
         return (
-            <Image src="/home/yasi/check-in-client/src/components/whosHere/staff/staff-user/cameron-venti-cpT1X2nPN2c-unsplash.jpg" roundedCircle/>
-        )
+            <Container>
+                <Row>
+                    <Col sm={8}>
+                        <h4>John Doe</h4>
+                        <h6>Developer</h6>
+                    </Col>
+                    <Col sm={4}>
+                        <Image src="/home/yasi/check-in-client/src/components/whosHere/staff/staff-user/cameron-venti-cpT1X2nPN2c-unsplash.jpg" roundedCircle fluid/>
+                    </Col>
+                </Row>
+            </Container>
+            )
     }
 }
