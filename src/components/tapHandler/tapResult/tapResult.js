5622f272645e7a5dbefefd6afdade0afea43509b
Taylor Berg <me@tman0.com>
2020-04-07 16:49:48 -0400

    don't render if visible = false

--- src/components/tapHandler/tapResult/tapResult.js
+++ src/components/tapHandler/tapResult/tapResult.js
@@ -7,71 +7,75 @@ import './tapResult.css'
 
 export class TapResult extends Component {
     render() {
-        let icon, color, leadMessage, lowerMessage;
+        if (this.props.visible) {
+            let icon, color, leadMessage, lowerMessage;
 
-        switch (this.props.result) {
-            case TapResult.result.enter:
-                icon = faCheck;
-                color = 'success';
-                leadMessage = 'Welcome';
-                break;
-            case TapResult.result.exit:
-                icon = faSignOutAlt;
-                color = 'success';
-                leadMessage = 'Goodbye';
-                break;
-            case TapResult.result.missingTrainings:
-                icon = faExclamationCircle;
-                color = 'warning';
-                leadMessage = 'Welcome';
-                lowerMessage = 'You are missing the following safety trainings: ' + this.props.user.missingTrainings;
-                break;
-            case TapResult.result.banned:
-                icon = faMinusCircle;
-                color = 'danger';
-                leadMessage = 'Do not enter';
-                lowerMessage = 'Your access to the lab has been revoked due to conduct violations. Please contact Idea Shop staff ' +
-                               'at ideashop@iit.edu for further information.';
-                break;
-            case TapResult.result.deniedFireCapacity:
-                icon = faMinusCircle;
-                color = 'danger';
-                leadMessage = 'Do not enter';
-                lowerMessage = 'The lab is currently at fire code capacity limits. Please come back later or check with staff.';
-                break;
-            case TapResult.result.deniedStaffRatio:
-                icon = faMinusCircle;
-                color = 'danger';
-                leadMessage = 'Do not enter';
-                lowerMessage = 'The lab is currently full. Please come back later or check with staff.';
-                break;
-            case TapResult.result.requireRegister:
-                return <Redirect to="/register" />
-            case TapResult.result.requireWaiver:
-                return <Redirect to="/waiver" />
-            default:
-                icon = faCheck;
-                color = 'success';
-                leadMessage = '';
-                lowerMessage = '';
-                break;
-        }
+            switch (this.props.result) {
+                case TapResult.result.enter:
+                    icon = faCheck;
+                    color = 'success';
+                    leadMessage = 'Welcome';
+                    break;
+                case TapResult.result.exit:
+                    icon = faSignOutAlt;
+                    color = 'success';
+                    leadMessage = 'Goodbye';
+                    break;
+                case TapResult.result.missingTrainings:
+                    icon = faExclamationCircle;
+                    color = 'warning';
+                    leadMessage = 'Welcome';
+                    lowerMessage = 'You are missing the following safety trainings: ' + this.props.user.missingTrainings;
+                    break;
+                case TapResult.result.banned:
+                    icon = faMinusCircle;
+                    color = 'danger';
+                    leadMessage = 'Do not enter';
+                    lowerMessage = 'Your access to the lab has been revoked due to conduct violations. Please contact Idea Shop staff ' +
+                                'at ideashop@iit.edu for further information.';
+                    break;
+                case TapResult.result.deniedFireCapacity:
+                    icon = faMinusCircle;
+                    color = 'danger';
+                    leadMessage = 'Do not enter';
+                    lowerMessage = 'The lab is currently at fire code capacity limits. Please come back later or check with staff.';
+                    break;
+                case TapResult.result.deniedStaffRatio:
+                    icon = faMinusCircle;
+                    color = 'danger';
+                    leadMessage = 'Do not enter';
+                    lowerMessage = 'The lab is currently full. Please come back later or check with staff.';
+                    break;
+                case TapResult.result.requireRegister:
+                    return <Redirect to="/register" />
+                case TapResult.result.requireWaiver:
+                    return <Redirect to="/waiver" />
+                default:
+                    icon = faCheck;
+                    color = 'success';
+                    leadMessage = '';
+                    lowerMessage = '';
+                    break;
+            }
 
-        return (
-            <div className="w-100 text-center">
-                <div className={'circle rounded-circle bg-' + color}>
-                    <FontAwesomeIcon icon={icon} className="circle-icon" />
+            return (
+                <div className="w-100 text-center">
+                    <div className={'circle rounded-circle bg-' + color}>
+                        <FontAwesomeIcon icon={icon} className="circle-icon" />
+                    </div>
+                    <div className="display-4">{leadMessage}</div>
+                    <div className="lead">{this.props.user.name}</div>
+                    {lowerMessage &&
+                    <React.Fragment>
+                        <hr />
+                        <div>{lowerMessage}</div>
+                    </React.Fragment>
+                    }
                 </div>
-                <div className="display-4">{leadMessage}</div>
-                <div className="lead">{this.props.user.name}</div>
-                {lowerMessage &&
-                <React.Fragment>
-                    <hr />
-                    <div>{lowerMessage}</div>
-                </React.Fragment>
-                }
-            </div>
-        )
+            )
+        } else {
+            return null;
+        }
     }
 }
 
